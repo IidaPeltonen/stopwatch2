3594476cf1e08df0ff57edf9db5865c1fa10a47f
IidaPeltonen <n0peii00@students.oamk.fi>
2021-09-29 13:03:06 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Clock from './clock';
+import Toggle from './toggle';
+
+function App() {
+  return (
+    <div className="App text-center" id="vari">
+     <Clock/>
+    </div>
+  );
+}
+
+export default App;
